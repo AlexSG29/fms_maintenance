74ed8175a5e26a7f8da4313abfa366dd80f03bb9
Alexander Sanchez G <alexander_sg29@outlook.com>
2024-06-10 13:06:13 -0500

    contact blank true

--- contact/models.py
+++ contact/models.py
@@ -10,10 +10,10 @@ class Contact(models.Model):
     name = models.CharField(max_length=255)
     contact_type = models.CharField(max_length=20, choices=CONTACT_TYPE_CHOICES)
     role = models.CharField(max_length=255)
-    description = models.TextField()
-    number = models.CharField(max_length=20)
-    email = models.EmailField()
-    address = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    number = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+    address = models.CharField(max_length=255, blank=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
