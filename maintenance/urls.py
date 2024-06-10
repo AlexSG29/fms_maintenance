83e7778ba91dcb767dfe9125a9c9a78a273c4daa
Alexander Sanchez G <alexander_sg29@outlook.com>
2024-06-10 18:00:02 -0500

    maintenance crud

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('list/', views.maintenance_view, name='maintenance_list'),
+    path('create/', views.maintenance_create_view, name='maintenance_create'),
+    path('maintenance/<int:maintenance_id>/edit/', views.maintenance_update_view, name='maintenance_edit'),
+    path('maintenance/<int:maintenance_id>/delete/', views.maintenance_delete_view, name='maintenance_delete'),
 ]
--- maintenance/views.py
+++ maintenance/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Maintenance
 from .forms import MaintenanceForm
 
@@ -9,7 +9,7 @@ def maintenance_view(request):
                   {'maintenances': maintenances}
                   )
 
-def contact_create_view(request):
+def maintenance_create_view(request):
     if request.method == 'POST':
         form = MaintenanceForm(request.POST)
         if form.is_valid():
@@ -20,4 +20,28 @@ def contact_create_view(request):
     return render(request, 
                   'maintenance_templates/maintenance_form.html', 
                   {'form': form}
+                  )
+
+def maintenance_update_view(request, maintenance_id):
+    maintenance = get_object_or_404(Maintenance, pk=maintenance_id)
+    if request.method == 'POST':
+        form = MaintenanceForm(request.POST, instance=maintenance)
+        if form.is_valid():
+            form.save()
+            return redirect(maintenance_view)  
+    else:
+        form = MaintenanceForm(instance=maintenance)
+    return render(request, 
+                  'maintenance_templates/maintenance_form.html', 
+                  {'form': form}
+                  )
+
+def maintenance_delete_view(request, maintenance_id):
+    maintenance = get_object_or_404(Maintenance, pk=maintenance_id)
+    if request.method == 'POST':
+        maintenance.delete()
+        return redirect(maintenance_view)  
+    return render(request, 
+                  'maintenance_templates/maintenance_confirm_delete.html', 
+                  {'maintenance': maintenance}
                   )
