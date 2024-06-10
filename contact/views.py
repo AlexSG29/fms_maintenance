be2d7e07ecaf2ade09ffdde53bea57d8fa8a3eba
Alexander Sanchez G <alexander_sg29@outlook.com>
2024-06-10 10:57:43 -0500

    contact rename template

--- contact/views.py
+++ contact/views.py
@@ -3,7 +3,7 @@ from .models import Contact
 
 
 def contact_view(request):
-    return render(request, 'contact_app_templates/contact_list.html')
+    return render(request, 'contact_templates/contact_list.html')
 
 """ def contacts_detail_view(request, contact_id):
     contact = Contact.objects.get(pk=contact_id)
