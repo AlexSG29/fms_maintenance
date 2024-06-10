5db59b8a659ab945cc0b01b016eef5c5a84ebb6c
Alexander Sanchez G <alexander_sg29@outlook.com>
2024-06-10 11:41:35 -0500

    create contact

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list/', views.contact_view, name='contact_view'),
+    path('create/', views.contact_create_view, name='contact_create_view'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
-
+from .forms import ContactForm
 
 def contact_view(request):
     contacts = Contact.objects.all()
@@ -9,3 +9,39 @@ def contact_view(request):
                   {'contacts': contacts}
                   )
 
+def contact_create_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(contact_view) 
+    else:
+        form = ContactForm()
+    return render(request, 
+                  'contact_templates/contact_create.html', 
+                  {'form': form}
+                  )
+
+""" def contact_update_view(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_templates/contact_form.html')  
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 
+                  'contact_templates/contact_form.html', 
+                  {'form': form}
+                  )
+
+def contact_delete_view(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_templates/contact_form.html')  
+    return render(request, 
+                  'contact_templates/contact_confirm_delete.html', 
+                  {'contact': contact}
+                  ) """
