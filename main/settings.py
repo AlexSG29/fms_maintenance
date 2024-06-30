45d2e6605f81d22ffe26ae21a81b2870a912d386
Alexander Sanchez G <alexander_sg29@outlook.com>
2024-06-30 10:02:08 -0500

    ahora si el template correcto

--- main/settings.py
+++ main/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Configuración de archivos estáticos
-STATIC_URL = '/assests/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assests'),
+    os.path.join(BASE_DIR, 'assets'),
 ]
 
 
